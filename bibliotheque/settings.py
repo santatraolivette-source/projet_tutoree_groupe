9ce7cf0e738df6038189da518692aa5cc8fe4bbf
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-16 21:43:49 +0300

    Just commit

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['cjn6a7oxmmozmail.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
