b34c63ba7d7a4cba373fbc2ad317ed1b8b8666ce
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-19 16:55:02 +0300

    Modifier allowed host

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cjn6a7oxmmozmail.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['projet-tutoree-groupe-2.onrender.com','127.0.0.1']
 
 
 # Application definition
